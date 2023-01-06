de77063a8ac99dd90c71cd6e12dcc4f06a9fa59f
danwritecode <dnelson4993@outlook.com>
2023-01-05 21:11:44 -0500

    wrapped list item in client only

--- ui/pages/[id].vue
+++ ui/pages/[id].vue
@@ -1,6 +1,6 @@
 <template>
   <ContainerNarrow class="pt-20">
-    <div v-if="timer.hours > 0" class="flex items-center space-x-3">
+    <div v-if="timer.hours > 0 && !riddle?.isSolved" class="flex items-center space-x-3">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7 text-gray-500">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
