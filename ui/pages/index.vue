3fad881867184cf0829e6862d2b32d1c75acbf48
danwritecode <dnelson4993@outlook.com>
2023-01-06 22:53:44 -0500

    made list item client only

--- ui/pages/index.vue
+++ ui/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <ContainerNarrow>
-    <div class="flex items-center justify-center h-screen">
-      <ol class="space-y-3">
-        <RiddleListItem v-for="riddle in riddles" :key="riddle.id" :riddle="riddle" />
-      </ol>
-    </div>
+    <ClientOnly>
+      <div class="flex items-center justify-center h-screen">
+        <ol class="space-y-3">
+          <RiddleListItem v-for="riddle in riddles" :key="riddle.id" :riddle="riddle" />
+        </ol>
+      </div>
+    </ClientOnly>
   </ContainerNarrow>
 </template>
 
