17d5d5596f6d08b1a2f4e8aa064beb7c28cad5f8
danwritecode <dnelson4993@outlook.com>
2023-01-06 22:49:05 -0500

    changed timezone

--- ui/server/api/riddles/index.get.ts
+++ ui/server/api/riddles/index.get.ts
@@ -1,5 +1,5 @@
 import { Riddle } from '../../../models/RiddlesResponse'
-import { $fetch, FetchError } from 'ohmyfetch'
+import { $fetch } from 'ohmyfetch'
 
 const config = useRuntimeConfig()
 
@@ -14,13 +14,16 @@ export default defineEventHandler(async (): Promise<Riddle[]> => {
 
 
   riddles.forEach(r => {
+    const currentDate = new Date()
+    currentDate.setHours(currentDate.getHours() - 5)
+
     console.log("Effective date renders as:")
     console.log(new Date(r.effective))
 
     console.log("current date renders as:")
-    console.log(new Date())
+    console.log(currentDate)
 
-    if(new Date(r.effective) < new Date()) {
+    if(new Date(r.effective) < currentDate) {
       r.isAvailable = true
     } else {
       r.isAvailable = false
