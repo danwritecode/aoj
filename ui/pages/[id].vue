1a8872c066f15aa633d99ac9f4ed7d6f344e3d11
danwritecode <dnelson4993@outlook.com>
2023-01-07 12:38:15 -0500

    updated to show solved text

--- ui/pages/[id].vue
+++ ui/pages/[id].vue
@@ -9,16 +9,23 @@
     <h1 class="mt-5 text-2xl font-medium text-gray-700">{{ riddle?.title }}</h1>
     <div class="mt-4 text-gray-600 prose max-w-none" v-html="riddle?.story"></div>
 
-    <h2 class="mt-6 font-medium">Riddle</h2>
-    <div v-if="!riddle?.isSolved" class="text-gray-600 prose max-w-none" v-html="riddle?.riddle"></div>
+    <div v-if="!riddle?.isSolved" class="mt-6">
+      <h2 class="font-medium">Riddle</h2>
+      <div class="text-gray-600 prose max-w-none" v-html="riddle?.riddle"></div>
+    </div>
 
-    <div v-if="riddle?.isSolved" class="mt-6 flex items-center space-x-2">
-      <p class="font-bold text-gray-700">You solved this riddle, here's your gold star:</p>
-      <span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-yellow-500">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-        </svg>
-      </span>
+    <div v-if="riddle?.isSolved" class="mt-8">
+      <div>
+        <div class="flex items-center space-x-2">
+          <p class="font-bold text-gray-700">You solved this riddle, here's your gold star:</p>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-yellow-500">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+            </svg>
+          </span>
+        </div>
+        <div class="mt-6 text-gray-500 prose max-w-none italic" v-html="riddle?.solvedText"></div>
+      </div> 
     </div>
     <div v-else-if="!riddle?.isSolved && timer.hours < 24">
       <div class="mt-6 sm:flex items-end sm:space-x-2">
